c469e9dd88621ba2b2c32d3451614e9b64e6b1ca
dudu <yeonyya@gmail.com>
2018-12-04 03:23:57 +0900

    배열의 동적생성
    ~p2-3

--- Ch02_기본자료구조/Ch02_기본자료구조/p2-1.c
+++ Ch02_기본자료구조/Ch02_기본자료구조/p2-1.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-
-#define N 5
-int main(void) {
-	int i;
-	int a[N];//배열 a의 자료형은 int[5]
-	for (int i = 0; i < N; i++)
-	{
-		printf("a[%d] : ", i);
-		scanf_s("%d", &a[i]);
-	}
-	puts("각 요소의 값");
-	for (i = 0; i < N; i++)
-	{
-		printf("a[%d] = %d\n", i, a[i]);
-	}
-
-	system("pause");
-	return 0;
-}
+//#include <stdio.h>
+//
+//#define N 5
+//int main(void) {
+//	int i;
+//	int a[N];//배열 a의 자료형은 int[5]
+//	for (int i = 0; i < N; i++)
+//	{
+//		printf("a[%d] : ", i);
+//		scanf_s("%d", &a[i]);
+//	}
+//	puts("각 요소의 값");
+//	for (i = 0; i < N; i++)
+//	{
+//		printf("a[%d] = %d\n", i, a[i]);
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ Ch02_기본자료구조/Ch02_기본자료구조/p2-2.c
@@ -0,0 +1,18 @@
+//#include <stdio.h>
+//
+//#define N 5
+//int main(void) {
+//	int i;
+//	int a[5] = { 1,2,3,4,5 }; //배열 선언과 동시에 초기화
+//	int na = sizeof(a) / sizeof(a[0]); //요소의 개수
+//	printf("배열 a의 요소개수는 %d입니다.\n", na);
+//
+//	int b[] = { 1,2,3,4,5,6 };
+//
+//	for (i = 0; i < na; i++) {
+//		printf("a[%d] = %d\n", i, a[i]);
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
